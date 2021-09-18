b80373e53fe1b566237e96ed170c60b9864ed397
Ninura.Jayasekara <ninura.jayasekara@gmail.com>
2021-09-18 18:08:18 +0530

    Commit Inferno Dog Care App

---
+++ app/src/main/java/com/example/inferno_dog_care_app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.inferno_dog_care_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.bmi_calculator);
+        setContentView(R.layout.main_menu);
+        setContentView(R.layout.appointment_details);
+        setContentView(R.layout.channelvet_choose);
+    }
+}
