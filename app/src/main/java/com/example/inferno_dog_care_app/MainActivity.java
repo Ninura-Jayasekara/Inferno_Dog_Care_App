6d113a691ce791d2529af5a293097267e2f09ff1
MishanPerera <mishkavinduperera65@gmail.com>
2021-09-18 22:24:10 +0530

    Commit Sell,Buy & Dog Walk Monitor

--- app/src/main/java/com/example/inferno_dog_care_app/MainActivity.java
+++ app/src/main/java/com/example/inferno_dog_care_app/MainActivity.java
@@ -9,9 +9,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.bmi_calculator);
+        setContentView(R.layout.bmi_calculator);
         setContentView(R.layout.main_menu);
         setContentView(R.layout.appointment_details);
         setContentView(R.layout.channelvet_choose);
+        setContentView(R.layout.view_sellpet);
+        setContentView(R.layout.buypet);
+        setContentView(R.layout.main_sellpet);
+        setContentView(R.layout.buy_profile);
+        setContentView(R.layout.dog_walk);
     }
 }
