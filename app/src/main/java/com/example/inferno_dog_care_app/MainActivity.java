aeacff63181efb4767dd0b57968ff34159ea773f
Ninura.Jayasekara <ninura.jayasekara@gmail.com>
2021-09-19 22:25:50 +0530

    Commit Ninura Splash Screen and Main Menu

--- app/src/main/java/com/example/inferno_dog_care_app/MainActivity.java
+++ app/src/main/java/com/example/inferno_dog_care_app/MainActivity.java
@@ -9,14 +9,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.bmi_calculator);
-        setContentView(R.layout.main_menu);
-        setContentView(R.layout.appointment_details);
-        setContentView(R.layout.channelvet_choose);
-        setContentView(R.layout.view_sellpet);
-        setContentView(R.layout.buypet);
-        setContentView(R.layout.main_sellpet);
-        setContentView(R.layout.buy_profile);
-        setContentView(R.layout.dog_walk);
+//        setContentView(R.layout.bmi_calculator);
+//        setContentView(R.layout.main_menu);
+//        setContentView(R.layout.appointment_details);
+//          setContentView(R.layout.channelvet_choose);
+//        setContentView(R.layout.view_sellpet);
+//        setContentView(R.layout.buypet);
+//        setContentView(R.layout.main_sellpet);
+//        setContentView(R.layout.buy_profile);
+//        setContentView(R.layout.dog_walk);
+        setContentView(R.layout.splash_screen);
     }
 }
