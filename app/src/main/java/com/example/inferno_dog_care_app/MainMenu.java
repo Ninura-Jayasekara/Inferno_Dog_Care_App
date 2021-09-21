cc0ac571aa7e0e9d8774902aa1b68cd1a6203f87
Ninura.Jayasekara <ninura.jayasekara@gmail.com>
2021-09-21 22:24:38 +0530

    Commit Main Menu Class

---
+++ app/src/main/java/com/example/inferno_dog_care_app/MainMenu.java
@@ -0,0 +1,12 @@
+package com.example.inferno_dog_care_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainMenu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+}}
