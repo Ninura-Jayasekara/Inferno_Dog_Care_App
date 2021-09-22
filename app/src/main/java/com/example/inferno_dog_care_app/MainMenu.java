04aefd47c3acd886e873a2d0eb5739bbb5d59ae6
Manuka-Dahanayake <manukaud98@gmail.com>
2021-09-23 00:20:31 +0530

    commit layouts

--- app/src/main/java/com/example/inferno_dog_care_app/MainMenu.java
+++ app/src/main/java/com/example/inferno_dog_care_app/MainMenu.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
 public class MainMenu  extends AppCompatActivity implements View.OnClickListener {
 
