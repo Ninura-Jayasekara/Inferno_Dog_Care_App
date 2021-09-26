4386b7ee2c25a1de69890b4441d13f2f893a6c6d
MishanPerera <mishkavinduperera65@gmail.com>
2021-09-26 08:03:58 +0530

    Commit Advertisement,Buy Pet, Buy Profile, View Advertisement, Dog Walk Monitor

---
+++ app/src/main/java/com/example/inferno_dog_care_app/Advertisement.java
@@ -0,0 +1,84 @@
+package com.example.inferno_dog_care_app;
+
+public class Advertisement {
+    String ID,Owner_Name,Phone_Number, Address,Breed,No_Of_Puppies,Birthday,Price;
+
+    public Advertisement(){
+
+    }
+
+    public Advertisement(String ID, String owner_Name, String phone_Number, String address, String breed, String no_Of_Puppies, String birthday, String price) {
+        this.ID = ID;
+        Owner_Name = owner_Name;
+        Phone_Number = phone_Number;
+        Address = address;
+        Breed = breed;
+        No_Of_Puppies = no_Of_Puppies;
+        Birthday = birthday;
+        Price = price;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getOwner_Name() {
+        return Owner_Name;
+    }
+
+    public void setOwner_Name(String owner_Name) {
+        Owner_Name = owner_Name;
+    }
+
+    public String getPhone_Number() {
+        return Phone_Number;
+    }
+
+    public void setPhone_Number(String phone_Number) {
+        Phone_Number = phone_Number;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getBreed() {
+        return Breed;
+    }
+
+    public void setBreed(String breed) {
+        Breed = breed;
+    }
+
+    public String getNo_Of_Puppies() {
+        return No_Of_Puppies;
+    }
+
+    public void setNo_Of_Puppies(String no_Of_Puppies) {
+        No_Of_Puppies = no_Of_Puppies;
+    }
+
+    public String getBirthday() {
+        return Birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        Birthday = birthday;
+    }
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String price) {
+        Price = price;
+    }
+}
---
+++ app/src/main/java/com/example/inferno_dog_care_app/AdvertisementAdapter.java
@@ -0,0 +1,83 @@
+package com.example.inferno_dog_care_app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdvertisementAdapter extends RecyclerView.Adapter<AdvertisementAdapter.AdvertisementViewHolder> {
+
+    Context context;
+    ArrayList<Advertisement> advertisementArrayList;
+
+    public AdvertisementAdapter(Context context, ArrayList<Advertisement> advertisementArrayList) {
+        this.context = context;
+        this.advertisementArrayList = advertisementArrayList;
+    }
+
+    @NonNull
+    @Override
+    public AdvertisementAdapter.AdvertisementViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.advertisement_item,parent,false);
+        return new AdvertisementViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdvertisementAdapter.AdvertisementViewHolder holder, int position) {
+        Advertisement advertisement = advertisementArrayList.get(position);
+
+        holder.Breed.setText(advertisement.Breed);
+        //holder.Price.setText(String.valueOf(user.Price));
+        holder.Price.setText(advertisement.Price);
+        String id =advertisement.ID;
+        String name =advertisement.Owner_Name;
+        String phone =advertisement.Phone_Number;
+        String address =advertisement.Address;
+        String breed =advertisement.Breed;
+        String count =advertisement.No_Of_Puppies;
+        String birthday =advertisement.Birthday;
+        String price =advertisement.Price;
+        holder.view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(v.getContext(),AdvertisementProfile.class);
+                intent.putExtra("ID",id);
+                intent.putExtra("Owner_Name",name);
+                intent.putExtra("Phone_Number",phone);
+                intent.putExtra("Address",address);
+                intent.putExtra("Breed",breed);
+                intent.putExtra("No_Of_Puppies",count);
+                intent.putExtra("Birthday",birthday);
+                intent.putExtra("Price",price);
+                context.startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return advertisementArrayList.size();
+    }
+
+    public static class AdvertisementViewHolder extends RecyclerView.ViewHolder{
+
+        TextView Breed,Price;
+        View view;
+
+        public AdvertisementViewHolder(@NonNull View itemView) {
+            super(itemView);
+            view = itemView;
+            Breed = itemView.findViewById(R.id.tvbreed);
+            Price = itemView.findViewById(R.id.tvprice);
+        }
+    }
+}
