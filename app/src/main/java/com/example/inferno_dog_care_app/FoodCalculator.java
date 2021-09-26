c71f2b566b716e9195b8a52a47fef18d93edc6da
Manuka-Dahanayake <manukaud98@gmail.com>
2021-09-26 10:19:40 +0530

    commit adoptdog,foodCalories,userProfile,userLogin, userRegister

--- app/src/main/java/com/example/inferno_dog_care_app/FoodCalculator.java
+++ app/src/main/java/com/example/inferno_dog_care_app/FoodCalculator.java
@@ -1,10 +1,28 @@
 package com.example.inferno_dog_care_app;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FoodCalculator  extends AppCompatActivity {
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.text.DecimalFormat;
+
+public class FoodCalculator extends AppCompatActivity{
+    private EditText txt_food_calories4;
+    private EditText txt_food_calories5;
+    private EditText txt_food_calories6;
+    private EditText txt_food_calories7;
+    private EditText txt_food_calories8;
+    private EditText txt_food_calories9;
+    private Button btn_food_calories;
+    private TextView txt_food_calories_colories2;
+
+
 
 
     @Override
@@ -12,5 +30,33 @@ public class FoodCalculator  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_calories);
 
+        txt_food_calories4 =  findViewById(R.id.txt_food_calories4);
+        txt_food_calories5 = findViewById(R.id.txt_food_calories5);
+        txt_food_calories6 = findViewById(R.id.txt_food_calories6);
+        txt_food_calories7 = findViewById(R.id.txt_food_calories7);
+        txt_food_calories8 = findViewById(R.id.txt_food_calories8);
+        txt_food_calories9 = findViewById(R.id.txt_food_calories9);
+        btn_food_calories =findViewById(R.id.btn_food_calories);
+        txt_food_calories_colories2=findViewById(R.id.txt_food_calories_colories2);
+        btn_food_calories.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                calculateCalorie();
+            }
+        });
+    }
+    private void calculateCalorie() {
+        float meat = Float.parseFloat(txt_food_calories4.getText().toString().trim());
+        float rice = Float.parseFloat(txt_food_calories5.getText().toString().trim());
+        float fish = Float.parseFloat(txt_food_calories6.getText().toString().trim());
+        float egg = Float.parseFloat(txt_food_calories7.getText().toString().trim());
+        float milk = Float.parseFloat(txt_food_calories8.getText().toString().trim());
+        float yogurt = Float.parseFloat(txt_food_calories9.getText().toString().trim());
+        float calorie = (float) ((meat * 2)+(rice * 1.3)+(fish * 2)+(egg * 155)+(milk * 0.4)+(yogurt * 59));
+
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        txt_food_calories_colories2.setText(""+df.format(calorie));
     }
 }
--- app/src/main/java/com/example/inferno_dog_care_app/MainMenu.java
+++ app/src/main/java/com/example/inferno_dog_care_app/MainMenu.java
@@ -56,7 +56,7 @@ public class MainMenu  extends AppCompatActivity implements View.OnClickListener
                 case R.id.buymain:i=new Intent(this,BuyPet.class);startActivity(i);break;
                 case R.id.walkdog:i=new Intent(this,DogWalkMonitor.class);startActivity(i);break;
                 case R.id.foodcal:i=new Intent(this,FoodCalculator.class);startActivity(i);break;
-                case R.id.adopt:i=new Intent(this,AdoptDog.class);startActivity(i);break;
+                case R.id.adopt:i=new Intent(this, AdoptDog.class);startActivity(i);break;
                 case R.id.login:i=new Intent(this,Login.class);startActivity(i);break;
 
 
